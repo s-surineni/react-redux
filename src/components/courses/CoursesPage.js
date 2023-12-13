0957b809795944f25ede6061d17064ccce98d02a
sampath surineni <ssurineni@salesforce.com>
2023-12-13 17:31:59 +0530

    added action for initial load

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,33 +5,19 @@ import PropTypes from 'prop-types';
 import { bindActionCreators } from 'redux';
 class CoursesPage extends React.Component {
 
-        state = {
-            course: {
-                title: ''
-            }
-        }
-
-
-    handleChange = (event) => {
-        const course = { ...this.state.course, title: event.target.value };
-        this.setState({ course });
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.props.actions.createCourse(this.state.course);
+    componentDidMount() {
+        this.props.actions.loadCourses().catch(error => {
+            alert("Loading courses failed" + error);
+        })
     }
     render() {
         return (
-            <form onSubmit={this.handleSubmit}>
+            <>
                 <h2>Courses</h2>
-                <h3>Add Course</h3>
-                <input type="text" value={this.state.course.title} onChange={this.handleChange} />
-                <input type="submit" value="Save" />
                 {this.props.courses.map((course) => (
                     <div key={course.title}>{course.title}</div>
                 ))}
-            </form>
+            </>
         );
     }
 }
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, createStore, compose } from "redux";
 import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
 import rootReducer from "./reducers";
+import thunk from "redux-thunk";
 
 export default function configureStore(initialState) {
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
     return createStore(
-        rootReducer, initialState, composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+        rootReducer, initialState, composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
     )
 }
