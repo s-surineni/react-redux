4583232140f3ec32ae7d8cbc8c5f0f77f100fe47
sampath surineni <ssurineni@salesforce.com>
2024-01-04 15:40:04 +0530

    reduce api call number on error

--- src/redux/reducers/apiStatusReducer.js
+++ src/redux/reducers/apiStatusReducer.js
@@ -7,7 +7,7 @@ function actionTypeEndsInSuccess(type) {
 export default function apiStatusReducer(state = initialState.apiCallsInProgress, action) {
     if (action.type === types.BEGIN_API_CALL) {
         return state + 1;
-    } else if (actionTypeEndsInSuccess(action.type)) {
+    } else if (action.type === types.API_CALL_ERROR || actionTypeEndsInSuccess(action.type)) {
         return state - 1;
     }
     return state;
